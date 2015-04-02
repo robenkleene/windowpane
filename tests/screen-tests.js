2bbbed5dd1e1b115054bbdb3596614987dec371e
Roben Kleene <robenk@gmail.com>
2015-04-02 19:33:50 -0400

    Test screen bounds

--- tests/screen-tests.js
+++ tests/screen-tests.js
@@ -2,7 +2,15 @@ var TestsHelper = require('./lib/test-helper.js');
 
 var Screen = require('../lib/screen.js');
 
+function testBounds() {
+  var bounds = Screen.bounds();
 
+  TestsHelper.assert(Object.keys(bounds).length == 4, "The screen's bounds should have four values.");
 
-var bounds = Screen.bounds();
-TestsHelper.logBounds(bounds);
+  for (var key in bounds) {
+    var value = bounds[key];
+    TestsHelper.assert(value > 0, "Every value of the screen's bounds.");
+  }
+}
+
+testBounds();
