4518f04dae24a762df5165e8c68b8466d8fd4e6a
Roben Kleene <robenk@gmail.com>
2015-04-01 22:26:16 -0400

    Test bounds equality

--- tests/lib/test-helper.js
+++ tests/lib/test-helper.js
@@ -1,6 +1,6 @@
 var TEST_APP_NAME = 'Finder';
 
-exports.closeEveryWindow = function closeEveryWindow() {
+exports.closeEveryWindow = function() {
 	var App = Application(TEST_APP_NAME);
 	var windows = App.windows;
 	for (count = App.windows.length, i = count - 1; i >= 0; i--) {
@@ -8,7 +8,7 @@ exports.closeEveryWindow = function closeEveryWindow() {
 	}
 }
 
-exports.makeWindowWithBounds = function makeWindowWithBounds(bounds) {
+exports.makeWindowWithBounds = function(bounds) {
 	var App = Application(TEST_APP_NAME);
   App.FinderWindow().make();
   var windowOne = App.windows[0];
@@ -16,6 +16,23 @@ exports.makeWindowWithBounds = function makeWindowWithBounds(bounds) {
 }
 
 exports.activate = function activate() {
-	var App = Application(TEST_APP_NAME);
+  var App = Application(TEST_APP_NAME);
   App.activate();
+}
+
+exports.boundsEqualsBounds = function(bounds1, bounds2) {
+  return (
+    bounds1.x == bounds2.x &&
+    bounds1.y == bounds2.y &&
+    bounds1.width == bounds2.width &&
+    bounds1.height == bounds2.height
+  );
+}
+
+// Debug
+exports.logBounds = function(bounds) {
+    console.log("x = " + bounds.x);
+    console.log("y = " + bounds.y);
+    console.log("width = " + bounds.width);
+    console.log("height = " + bounds.height);
 }
--- tests/tests.js
+++ tests/tests.js
@@ -2,13 +2,17 @@ var TestsHelper = require('./lib/test-helper.js');
 var WindowHelper = require('../lib/window-helper.js');
 var TestConstants = require('./lib/test-constants.js');
 
-function testFocusedWindow() {
+function setUp() {
   TestsHelper.activate();
-  TestsHelper.closeEveryWindow();
+  TestsHelper.closeEveryWindow();  
+}
+
+function testFocusedWindow() {
+  setUp();
   TestsHelper.makeWindowWithBounds(TestConstants.testWindowBounds);
   var focusedWindow = WindowHelper.getFocusedWindow();
-
-  console.log(Object.keys(focusedWindow.bounds()));
+  var result = TestsHelper.boundsEqualsBounds(focusedWindow.bounds(), TestConstants.testWindowBounds);
+console.log("result = " + result)
 }
 
 testFocusedWindow();
