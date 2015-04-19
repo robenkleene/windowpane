2a8bcda6be8fbbf34125780269dfa8ad1737d96a
Roben Kleene <robenk@gmail.com>
2015-04-19 15:26:59 -0400

    Apply window bounds hack

--- tests/window-manager-tests.js
+++ tests/window-manager-tests.js
@@ -1,6 +1,9 @@
 var TestHelper = require('./lib/test-helper');
 var TestConstants = require('./lib/test-constants');
+var WindowTestHelper = require('./lib/window-test-helper')
+
 var WindowManager = require('../lib/window-manager');
+var Window = require('../lib/window');
 
 var windowManager;
 
@@ -28,13 +31,23 @@ function testEmptyConstructor() {
   TestHelper.assert(windowManager.grid.verticalBlocks == TestConstants.defaultVerticalBlocks, "The new grid should have the default number of vertical blocks");
 }
 
+function testBoundsMovedDown() {
+  windowManager = new WindowManager(null, null, mockScreen);
+  var bounds = windowManager.boundsMovedDown(TestConstants.testWindowBounds); 
+  var result = TestHelper.boundsEqualsBounds(bounds, TestConstants.testWindowBoundsMovedDown);
+  TestHelper.assert(result, "The bounds should equal the test window bounds moved down");  
+}
+
 function testMoveWindowDown() {
   setUp();
   WindowTestHelper.makeWindowWithBounds(TestConstants.testWindowBounds);
   windowManager.moveFocusedWindowDown();
+  var testWindowBoundsHacked = windowManager.grid.boundsHacked(TestConstants.testWindowBoundsMovedDown);
+  var result = TestHelper.boundsEqualsBounds(Window.focusedWindow().bounds(), testWindowBoundsHacked);
+  TestHelper.assert(result, "The windows bounds should equal the hacked test window bounds moved down");  
   tearDown();
 }
 
-
 testEmptyConstructor();
-// testMoveWindowDown();
+testBoundsMovedDown();
+testMoveWindowDown();
